00e8ed5c19749e49648e997b55f2399c80b5ebfa
Himanshu Yadav <bunty.him@live.com>
2022-01-14 11:51:13 +0530

    Updated nor_representation.c
    A NOR function has been added instead of its boolean equivalent statements.
    Few Cosmetic changes were also added

--- Assignment_1/nor_representation.c
+++ Assignment_1/nor_representation.c
@@ -4,29 +4,36 @@
 
 #include<stdio.h>
 
+unsigned char NOR(unsigned char X, unsigned char Y) //NOR function
+{
+    return ~(X|Y); 
+}
+
 int main()
 {
-    unsigned char  num ;                            //input number
-    unsigned char  A=0x00,B=0x00,C=0x00,D=0x00;	    //binary inputs
+    unsigned char num ;                             //input number
+    unsigned char A=0x00,B=0x00,C=0x00,D=0x00;	    //binary inputs
     unsigned char one = 0x01;
-    unsigned char Org,Nor;                          //outputs 
+    unsigned char Org, Nor;                         //outputs 
 
-    printf("Binary Input  (A+B)(C+D)  ((A+B)'+(C+D)')'\n");
+    printf("Binary Input  (A+B)(C+D)  NOR Equivalent\n");
 
-    for (num = 0x00; num<0x10; num++) {             //loop to iterate through all usecases
-
+    for (num = 0x00; num<0x10; num++)               //loop to iterate through all usecases
+    {
         A = num>>3;    B = num>>2;    C = num>>1;    D = num>>0; //changing the inputs , D is LSB
         
         Org = (A|B)&(C|D);                          //Original Boolean Function
-        Nor = ~((~(A|B))|(~(C|D)));                 //NOR gate equivalent Boolean Function
+        Nor = NOR( NOR(A, B), NOR(C, D) );          //NOR gate equivalent Boolean Function
        
         printf("  %x %x %x %x", one&A, one&B, one&C, one&D);    //Input ABCD
         printf("\t\t%x\t\t\t%x\n" , one&Org, one&Nor);          //Output Org, Nor
     
     }
-
+    
+    return 0;  
+}
     /*Output
-        Binary Input  (A+B)(C+D)  ((A+B)'+(C+D)')'
+        Binary Input  (A+B)(C+D)   NOR Equivalent
         0 0 0 0	        0		    0
         0 0 0 1	        0		    0
         0 0 1 0	        0		    0
@@ -44,6 +51,3 @@ int main()
         1 1 1 0	        1		    1
         1 1 1 1	        1		    1
     */
-    
-    return 0;
-}
